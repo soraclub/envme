f434e2e1e3ab99e3e3ec574202d5f8e18dd5952f
guanzi <hlxf0330@163.com>
2018-11-08 21:48:10 +0800

    update

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-prj_home=$(readlink -n -f $0 | xargs dirname | xargs dirname)
+prj_home=$(readlink -n -f $0 | xargs dirname)
 
 # shell
 for conf in bash_profile gitconfig on-my-zsh shrc tmux.conf zshrc
@@ -7,17 +7,28 @@ do
     if [[ -f $HOME/.${conf} ]]; then
         mv $HOME/.${conf} $HOME/.${conf}.bak
     fi
+    if [[ -L $HOME/.${conf} ]]; then
+        unlink $HOME/.${conf} 
+    fi
     ln -s ${prj_home}/shell/${conf} $HOME/.${conf}
 done
 
 # zsh theme
 ln -s $prj_home/shell/me-ys.zsh-theme $prj_home/shell/oh-my-zsh/themes/
 
+# check which
+if [[ -z $(whereis which | cut -d':' -f 2) ]]; then 
+    echo "you need which: sudo yum install -y which"
+    exit
+fi
+
+
 # checks 
 for tool in zsh tmux vim 
 do
     if [[ -z $(which $tool 2>/dev/null) ]]; then
         echo "you need zsh: sudo yum install -y $tool"
+        exit
     fi
 done
 
@@ -31,6 +42,7 @@ if [[ ! -z $(which vim) ]]; then
         ;;
         *)
             echo "i do not know your vim"
+            exit
         ;;
     esac
 fi
